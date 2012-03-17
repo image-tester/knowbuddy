9bf7ff3130a335cc5ae78ae8151b1548af05b60d
Rohan Daxini <rohan_daxini@yahoo.com>
2012-03-17 14:10:12 +0530

    send email notification upon successful singup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :kyu_entries
+  after_create :send_welcome_email 
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  private
+  
+      #Added by Rohan. Functionality - Send email notification to user upon new account signup
+      def send_welcome_email
+        UserMailer.welcome_email(self).deliver 
+      end
 end

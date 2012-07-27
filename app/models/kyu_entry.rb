bd80250b4df9c485403378c1af54f792ff93c4b6
swatijadhav <swati.jadhav@kiprosh.com>
2012-07-27 14:28:38 +0530

    added friendly IDs for KYU Entries

--- app/models/kyu_entry.rb
+++ app/models/kyu_entry.rb
@@ -1,8 +1,11 @@
 class KyuEntry < ActiveRecord::Base
+   extend FriendlyId
+   friendly_id :subject, :use => :slugged
    acts_as_taggable_on :tags
    has_many :comments, :dependent => :destroy
    belongs_to :user
-    validates_presence_of :content, :subject
-    
-    paginates_per 10
+   validates_presence_of :content, :subject, :slug
+
+   paginates_per 10
+
 end

4a28335de8cc159c6c14af4a06489111dd60b11b
swatijadhav <swati.jadhav@kiprosh.com>
2012-08-06 18:38:23 +0530

    ordered contributors list

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $("#formID").validationEngine();
-   });
+    var mylist = $('#namelist');
+    var listitems = mylist.children('li').get();
+    listitems.sort(function(a, b) {
+      return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+    })
+    $.each(listitems, function(idx, itm) { mylist.append(itm); });
+});
 

1c5a0164ce0ee483508ef3a690c7dee4494d9a6d
Yatish  Hejmadi <yatishpoojary@ymail.com>
2012-08-06 20:44:14 +0530

    highlight current menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
+
+  def is_active?(page_name)
+    "menu_active" if params[:action] == page_name
+  end
 end

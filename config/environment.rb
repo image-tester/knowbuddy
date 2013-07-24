ee75db8a64fcc4be7c8232df9a07c4e260a377c1
root <root@li346-20.members.linode.com>
2013-07-24 08:51:40 -0400

    config updates

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,23 @@ require File.expand_path('../application', __FILE__)
 APP_CONFIG = YAML::load(File.open("#{Rails.root}/config/appconfig.yml"))[Rails.env]
 
 # Added by Rohan - set smtp host configuration
+#SMTP_SETTINGS = {
+#  address:                 "smtp.bizmail.yahoo.com",
+#  port:                    465,
+#  domain:                  'kiprosh.com',
+#  authentication:          'plain',
+#  user_name:               'careers@kiprosh.com',
+#  password:                'careers0987',
+#  enable_starttls_auto:    true }
+
 SMTP_SETTINGS = {
-  address:                 "mail.oncocure.com",
-  port:                    25,
-  domain:                  'mail.oncocure.com',
-  user_name:               'postmaster@oncocure.com',
-  password:                'oncomail1234',
-  authentication:          'plain',
-  enable_starttls_auto:    true  }
+  address:              'smtp.sendgrid.net',
+  port:                  587,
+  domain:               'knowbuddy.kiprosh-app.com',
+  user_name:            'shivanibhanwal',
+  password:             'd0n0tmess',
+  authentication:       'plain',
+  enable_starttls_auto:  true  }
 
 # Initialize the rails application
 KYU::Application.initialize!

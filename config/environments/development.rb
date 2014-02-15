9036348688216f2a5cec59f1caab09f37b4f8f04
Rohan Daxini <rohan_daxini@yahoo.com>
2014-02-15 18:34:11 +0530

    enable config to raise email delivery errors in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ KYU::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
   # Only use best-standards-support built into browsers

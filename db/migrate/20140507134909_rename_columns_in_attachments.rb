167a54401313021f56a2e084decc90b635d23d09
sunil-sharma <sunil.sharma@kiprosh.com>
2014-05-07 20:16:23 +0530

    migration to update kyu column in Attachment table

---
+++ db/migrate/20140507134909_rename_columns_in_attachments.rb
@@ -0,0 +1,8 @@
+class RenameColumnsInAttachments < ActiveRecord::Migration
+  def change
+  	rename_column :attachments, :kyu_file_name, :post_file_name
+    rename_column :attachments, :kyu_file_size, :post_file_size
+    rename_column :attachments, :kyu_content_type, :post_content_type
+    rename_column :attachments, :kyu_updated_at, :post_updated_at
+  end
+end

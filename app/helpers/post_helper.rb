319e20046332843627c1b21269e6377fc23f1867
Amol Shinde <amol.shinde@kiprosh.com>
2014-09-12 20:28:57 +0530

    Refactor Contributors Pagination

---
+++ app/helpers/post_helper.rb
@@ -0,0 +1,23 @@
+module PostHelper
+
+  def view_more_activities_link(next_page)
+    link_to 'View More', load_activities_posts_path(page_3: next_page), remote: true,
+      id: "load_more_link"
+  end
+
+  def user_link(user, top = false)
+    link_to "#{user.display_name} #{post_count(user, top)}",
+      user_posts_posts_path(user_id: user.id), remote: true
+  end
+
+  def post_count(user, top)
+     top ? "(#{user.total})" : ""
+  end
+
+  def post_date_link(post)
+    link_to (timeago_date_format(post.updated_at)),
+      post_date_posts_path(post_id: post.id),remote: true
+  end
+
+end
+

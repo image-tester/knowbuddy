605f1dfe803d44ee0aee6f3175286ba278d3f07b
yogendra689 <yogendra.tala@kiprosh.com>
2014-11-12 20:44:06 +0530

    disable if already liked

--- app/assets/javascripts/like_post.js
+++ app/assets/javascripts/like_post.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var post_id = $("#post-subject").data('id');
 
-  $('body').on('click','.voterDetails span i', function(event) {
+  $('body').on('click',".voterDetails i:not('.voted')", function(event) {
     $.ajax({
       type: "POST",
       dataType: "JSON",
@@ -11,7 +11,7 @@ $(document).ready(function(){
       success: function(data){
         $(".up_rate .vote").html(data["likes"]);
         $(".down_rate .vote").html(data["dislikes"]);
-        $(".voterDetails span i").removeClass('voted');
+        $(".voterDetails i").removeClass('voted');
         $(event.target).addClass('voted');
       }
     });

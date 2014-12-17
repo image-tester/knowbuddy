3fbc0c00ef15651bbda081e0f34cb135245c8c06
namrataukirde <namrata.ukirde@kiprosh.com>
2014-12-17 20:55:00 +0530

    config file changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KYU::Application.config.secret_token = 'ea9658119d390282b615598799a34dbcf140\
-                                       d76f959fc2ee8ad5567a82ef2aea8b95d51b\
-                                       b81b82c63c9b4653488908b0102ad8012057\
-                                       7873ab7a8c7fc5ca5988'
+KYU::Application.config.secret_key_base = 'ea9658119d390282b615598799a34dbcf1
+                                           40\d76f959fc2ee8ad5567a82ef2aea8b95d51b\b81b82c63c9b4653488908b0102ad8012057\7873ab7a8c7fc5ca5988'

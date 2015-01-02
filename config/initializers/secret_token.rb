94195d5f2cae72e93758c9d87042fe00e8541d7e
namrataukirde <namrata.ukirde@kiprosh.com>
2015-01-02 19:45:25 +0530

    config changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 KYU::Application.config.secret_key_base = 'ea9658119d390282b615598799a34dbcf1
-                                           40\d76f959fc2ee8ad5567a82ef2aea8b95d51b\b81b82c63c9b4653488908b0102ad8012057\7873ab7a8c7fc5ca5988'
+  40\d76f959fc2ee8ad5567a82ef2aea8b95d51b\b81b82c63c9b4653488908b0102ad8012057\7873ab7a8c7fc5ca5988'

d6e3780574d58b8c9447fc85659a631b6a4d111b
titame <tushar.titame@kiprosh.com>
2015-04-07 13:56:08 +0530

    hound warnings fixed

--- app/admin/rule_engine.rb
+++ app/admin/rule_engine.rb
@@ -1,10 +1,9 @@
 ActiveAdmin.register RuleEngine, as: "Rule" do
-
   filter :rule, label: "Rule"
 
   index do
     column :rule do |c|
-      truncate( c.rule, length: 40)
+      truncate(c.rule, length: 40)
     end
     column :rule_for
     column :min_count
@@ -23,24 +22,31 @@ ActiveAdmin.register RuleEngine, as: "Rule" do
   form do |f|
     f.inputs "Details" do
       f.input :rule
-      f.input :rule_for, collection: [["Post","post"]],
-        include_blank: "-Select-", input_html: { class: "dropdown rule_for" }
+      f.input :rule_for, collection: [["Post", "post"]],
+        include_blank: "-Select-",
+        input_html: { class: "dropdown rule_for" }
       f.input :min_count, as: :number, min: 0,
         input_html: { class: "min_count" }
       f.input :max_count, as: :number, min: 1,
         input_html: { class: "min_count" }
       f.input :frequency, collection: RuleEngine.frequency_array,
-        include_blank: "-Select-", input_html: { class: "dropdown" }
+        include_blank: "-Select-",
+        input_html: { class: "dropdown" }
       f.input :schedule, collection: Date::DAYNAMES,
-        include_blank: false, input_html: { class: "dropdown schedule_input" }
+        include_blank: false,
+        input_html: { class: "dropdown schedule_input" }
       f.input :max_duration, collection: RuleEngine.duration_array,
-        include_blank: false, input_html: { class: "dropdown schedule_input" }
-      f.input :subject, as: :text, input_html: { class: "text" }
-      f.input :body, as: :text, input_html: { class: "text" }
+        include_blank: false,
+        input_html: { class: "dropdown schedule_input" }
+      f.input :subject, as: :text,
+        input_html: { class: "text" }
+      f.input :body, as: :text,
+        input_html: { class: "text" }
       f.input :active
       f.actions
     end
   end
 
-  permit_params :frequency, :subject, :body, :min_count, :max_count, :rule, :rule_for, :max_duration, :schedule, :active
+  permit_params :frequency, :subject, :body, :min_count, :max_count,
+    :rule, :rule_for, :max_duration, :schedule, :active
 end
